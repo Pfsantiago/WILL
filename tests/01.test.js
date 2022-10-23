43f14c484e8f139c2ddb4ff5335ad863d90e5cf8
Pfsantiago <113811659+Pfsantiago@users.noreply.github.com>
2022-10-23 10:22:55 -0500

    Update 01.test.js
             function describir (numero, palabra) {
      describir (1, ´soy´, 2, ´yo´, 3, ´Henry´)  
    }
      it('should return [1, 2] for [1, "Henry", 2]', function() {
          expect(soloNumeros([1, 'Henry', 2])).toStrictEqual([1, 2]);
      });
      it('should return [1, 3, 2] for [1, "Soy", 3, "Henry", 2]', function() {
        expect(soloNumeros([1,'Soy', 3, 'Henry', 2])).toStrictEqual([1, 3, 2]);
      });
    });

--- tests/01.test.js
+++ tests/01.test.js
@@ -1,6 +1,9 @@
 const soloNumeros = require('../01.js');
 
-describe('soloNumeros', function() {
+describe('soloNumeros', function()
+         function describir (numero, palabra) {
+  describir (1, ´soy´, 2, ´yo´, 3, ´Henry´)  
+}
   it('should return [1, 2] for [1, "Henry", 2]', function() {
       expect(soloNumeros([1, 'Henry', 2])).toStrictEqual([1, 2]);
   });
@@ -8,3 +11,5 @@ describe('soloNumeros', function() {
     expect(soloNumeros([1,'Soy', 3, 'Henry', 2])).toStrictEqual([1, 3, 2]);
   });
 });
+
+
